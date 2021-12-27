f9523cb03b188503b2209654cbdaeea914021d5c
Adilet <berdibekovadilet@icloud.com>
2021-12-27 15:37:46 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Adilet Berdibekov on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
